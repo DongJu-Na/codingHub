2d85ae3713b608c4534a087fe1411f2c1d593586
ndj <ehdeld123@naver.com>
2024-03-15 20:50:27 +0900

    백준 1000
    A+B

---
+++ src/main/java/org/example/basic/BOJ1000.java
@@ -0,0 +1,23 @@
+package org.example.basic;
+
+import java.util.Scanner;
+
+/*
+* https://www.acmicpc.net/problem/1000
+* Q 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+* I 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+* O 첫째 줄에 A+B를 출력한다.
+* EI 1 2
+* EO 3
+* */
+public class BOJ1000 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        System.out.println(num1 + num2);
+
+    }
+}
