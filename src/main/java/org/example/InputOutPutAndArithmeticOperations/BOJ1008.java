c5058a45cf570ab9692c022946b449fb41d43114
ndj <ehdeld123@naver.com>
2024-03-27 19:52:52 +0900

    백준 1008

---
+++ src/main/java/org/example/InputOutPutAndArithmeticOperations/BOJ1008.java
@@ -0,0 +1,16 @@
+package org.example.InputOutPutAndArithmeticOperations;
+
+import java.util.Scanner;
+
+public class BOJ1008 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        sc.close();
+        System.out.println(a / b);
+    }
+
+}
