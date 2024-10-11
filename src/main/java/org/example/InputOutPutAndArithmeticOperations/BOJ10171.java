c1a0fd9c52fc0e915f07cb3d902d5415ed3e7607
DongJu-Na <ehdeld123@naver.com>
2024-10-11 10:49:20 +0900

    백준 10171

---
+++ src/main/java/org/example/InputOutPutAndArithmeticOperations/BOJ10171.java
@@ -0,0 +1,12 @@
+package org.example.InputOutPutAndArithmeticOperations;
+
+public class BOJ10171 {
+
+    public static void main(String[] args) {
+            System.out.println("\\    /\\");
+            System.out.println(" )  ( ')");
+            System.out.println("(  /  )");
+            System.out.println(" \\(__)|");
+    }
+
+}
